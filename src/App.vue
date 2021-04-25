0056b03f69a88678e9a808dc6833573f92be6fc4
Madeleine Hill <madeleine.hill1@mohawkcollege.ca>
2021-04-24 23:21:33 -0400

    Remove hello-world defaults

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Collection msg="Nail Polish App"/>
+  <img alt="Ethereal Chill" src="./assets/ethereal_chill.jpg">
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Collection from './components/Collection.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Collection
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+img {
+  width: 300px;
 }
 </style>
---
+++ src/components/Collection.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>Coming soon!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Collection',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
