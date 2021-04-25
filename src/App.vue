857f0686104ce7f35071fc8996fa9d62893c8079
Madeleine Hill <madeleine.hill1@mohawkcollege.ca>
2021-04-25 13:27:21 -0400

    Add icons to navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
-    <b-navbar>
-      <b-navbar-brand tag="h1" class="mb-0">Nail Polish App</b-navbar-brand>
+    <b-navbar sticky="true" variant="light">
+      <b-navbar-brand tag="h1">Nail Polish App</b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item id="link-button" tabindex="0">
+          <font-awesome-icon icon="info-circle" />
+        </b-nav-item>
+        <b-popover variant="info" target="link-button" title="Info" triggers="focus">
+          An application to track my collection of nail polishes and showcase some of my favourite nail art looks.
+        </b-popover>
+        <b-nav-item href="https://mh11wi.github.io/">
+          <font-awesome-icon icon="home" />
+        </b-nav-item>
+      </b-navbar-nav>
     </b-navbar>
     <Collection msg="Coming soon!"/>
     <img alt="Ethereal Chill" src="./assets/ethereal_chill.jpg">
--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faInfoCircle, faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faInfoCircle, faHome)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 
