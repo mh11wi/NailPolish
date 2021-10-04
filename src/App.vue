dd8fa8456ae9dd5fcb02447db03522911bc3c18c
Madeleine Hill <madeleine.hill1@mohawkcollege.ca>
2021-10-03 22:01:48 -0400

    Change wording back

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
           <font-awesome-icon icon="info-circle" size="lg"/>
         </b-nav-item>
         <b-popover variant="info" target="info-link" title="App Info" triggers="click blur">
-          <p>I created this application to track my collection of nail polishes and showcase some of my favourite nail art designs.</p>
+          <p>I created this application to track my collection of nail polishes and showcase some of my favourite nail art looks.</p>
           <p><strong>Browse Collection</strong><br>Search or filter through my collection of nail polishes.</p>
           <p><strong>Compare Polishes</strong><br>Compare similar polishes next to one another.</p>
           <p><strong>Top It Off</strong><br>See what different toppers look like over an applicable polish.</p>
