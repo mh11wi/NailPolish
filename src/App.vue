c4fd2c8bbb72cdb055b0a248b0c44905c90251c2
Madeleine Hill <mh11wi@outlook.com>
2023-03-23 22:51:29 -0400

    Add privacy policy link

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,11 @@
           <p><strong>Nail Art Gallery</strong><br>View some of my past nail art.</p>
           <p><strong>Polish Guesser</strong><br>A 'Wordle' inspired game!</p>
           <penguin width="100" hAlign="center"/>
-          <p class="text-right">Thanks for visiting!<br>&ndash;mads</p>
+          <div class="text-center">
+            Thanks for visiting! &ndash;mads
+            <br>
+            <a href="https://mh11wi.github.io/privacy-policy.html">Terms & Privacy Policy</a>
+          </div>
         </b-popover>
         <b-nav-item href="https://mh11wi.github.io/" :link-attrs="{'aria-label': 'Home'}">
           <font-awesome-icon icon="home" size="lg"/>
@@ -219,6 +223,12 @@ html, body, .app {
 .b-popover-info .popover-header {
   color: #0c5460 !important;
   font-weight: 500;
+}
+
+.b-popover-info a {
+  color: #0c5460;
+  font-size: 85%;
+  text-decoration: underline;
 }
 
 .modal-dialog h2 {
