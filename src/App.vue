a22f6727a2b3d6a1aeaf85139dfc96f627ec1624
Madeleine Hill <mh11wi@outlook.com>
2023-05-03 17:14:50 -0400

    Change tag To implicitly fix padding in info card.

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
           <p><strong>Nail Art Gallery</strong><br>View some of my past nail art.</p>
           <p><strong>Polish Guesser</strong><br>A 'Wordle' inspired game!</p>
           <penguin width="100" hAlign="center"/>
-          <p class="text-center"><a href="https://mh11wi.github.io/privacy-policy.html">Terms & Privacy Policy</a></p>
+          <div class="text-center"><a href="https://mh11wi.github.io/privacy-policy.html">Terms & Privacy Policy</a></div>
         </b-popover>
         <b-nav-item href="https://mh11wi.github.io/" :link-attrs="{'aria-label': 'Home'}">
           <font-awesome-icon icon="home" size="lg"/>
